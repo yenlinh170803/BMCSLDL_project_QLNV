1f202214f654086539ac76a03b2883de9b1a8a40
Yen Linh <Yen Linh@ADMIN>
2024-05-05 15:07:05 +0700

    frm_quanlytructiep

--- Quan_ly_NV/Program.cs
+++ Quan_ly_NV/Program.cs
@@ -16,7 +16,7 @@ namespace Quan_ly_NV
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frm_truongdean());
+            Application.Run(new frm_quanlytructiep());
         }
     }
 }
