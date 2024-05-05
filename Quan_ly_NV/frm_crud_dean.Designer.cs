bcc4fc68170b4a99c238000579d4a5a1838523c5
Yen Linh <Yen Linh@ADMIN>
2024-05-05 15:02:38 +0700

    frm_trgdenan

---
+++ Quan_ly_NV/frm_crud_dean.Designer.cs
@@ -0,0 +1,338 @@
+﻿namespace Quan_ly_NV
+{
+    partial class frm_crud_dean
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_capnhat = new System.Windows.Forms.Button();
+            this.btn_them = new System.Windows.Forms.Button();
+            this.txt_themmadean = new System.Windows.Forms.TextBox();
+            this.lbl_capnhat = new System.Windows.Forms.Label();
+            this.lbl_them = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbl_capnhattendean = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.lbl_capnhatphong = new System.Windows.Forms.Label();
+            this.cb_capnhatmadean = new System.Windows.Forms.ComboBox();
+            this.lbl_capnhatmadean = new System.Windows.Forms.Label();
+            this.lbl_themtendean = new System.Windows.Forms.Label();
+            this.comboBox10 = new System.Windows.Forms.ComboBox();
+            this.lbl_themphong = new System.Windows.Forms.Label();
+            this.lbl_themmadean = new System.Windows.Forms.Label();
+            this.txt_xoamadean = new System.Windows.Forms.TextBox();
+            this.lbl_xoa = new System.Windows.Forms.Label();
+            this.lbl_xoamadean = new System.Windows.Forms.Label();
+            this.lbl_capnhatngaybd = new System.Windows.Forms.Label();
+            this.lbl_themngaybd = new System.Windows.Forms.Label();
+            this.dtP_themngaybd = new System.Windows.Forms.DateTimePicker();
+            this.dtP_capnhatngaybd = new System.Windows.Forms.DateTimePicker();
+            this.btn_xoa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_capnhat
+            // 
+            this.btn_capnhat.BackgroundImage = global::Quan_ly_NV.Properties.Resources.backgroud;
+            this.btn_capnhat.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_capnhat.FlatAppearance.BorderSize = 0;
+            this.btn_capnhat.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_capnhat.Location = new System.Drawing.Point(635, 402);
+            this.btn_capnhat.Name = "btn_capnhat";
+            this.btn_capnhat.Size = new System.Drawing.Size(112, 38);
+            this.btn_capnhat.TabIndex = 59;
+            this.btn_capnhat.Text = "Cập nhật";
+            this.btn_capnhat.UseVisualStyleBackColor = true;
+            // 
+            // btn_them
+            // 
+            this.btn_them.BackgroundImage = global::Quan_ly_NV.Properties.Resources.backgroud;
+            this.btn_them.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_them.FlatAppearance.BorderSize = 0;
+            this.btn_them.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_them.Location = new System.Drawing.Point(90, 402);
+            this.btn_them.Name = "btn_them";
+            this.btn_them.Size = new System.Drawing.Size(116, 38);
+            this.btn_them.TabIndex = 58;
+            this.btn_them.Text = "Thêm";
+            this.btn_them.UseVisualStyleBackColor = true;
+            // 
+            // txt_themmadean
+            // 
+            this.txt_themmadean.Location = new System.Drawing.Point(74, 111);
+            this.txt_themmadean.Name = "txt_themmadean";
+            this.txt_themmadean.Size = new System.Drawing.Size(166, 20);
+            this.txt_themmadean.TabIndex = 57;
+            // 
+            // lbl_capnhat
+            // 
+            this.lbl_capnhat.AutoSize = true;
+            this.lbl_capnhat.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_capnhat.Location = new System.Drawing.Point(639, 50);
+            this.lbl_capnhat.Name = "lbl_capnhat";
+            this.lbl_capnhat.Size = new System.Drawing.Size(91, 25);
+            this.lbl_capnhat.TabIndex = 56;
+            this.lbl_capnhat.Text = "Cập nhật";
+            // 
+            // lbl_them
+            // 
+            this.lbl_them.AutoSize = true;
+            this.lbl_them.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_them.Location = new System.Drawing.Point(122, 50);
+            this.lbl_them.Name = "lbl_them";
+            this.lbl_them.Size = new System.Drawing.Size(61, 25);
+            this.lbl_them.TabIndex = 55;
+            this.lbl_them.Text = "Thêm";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(603, 178);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 62);
+            this.textBox2.TabIndex = 54;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(74, 181);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(166, 62);
+            this.textBox1.TabIndex = 53;
+            // 
+            // lbl_capnhattendean
+            // 
+            this.lbl_capnhattendean.AutoSize = true;
+            this.lbl_capnhattendean.Location = new System.Drawing.Point(528, 181);
+            this.lbl_capnhattendean.Name = "lbl_capnhattendean";
+            this.lbl_capnhattendean.Size = new System.Drawing.Size(57, 13);
+            this.lbl_capnhattendean.TabIndex = 52;
+            this.lbl_capnhattendean.Text = "Tên đề án";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(607, 331);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(172, 21);
+            this.comboBox2.TabIndex = 51;
+            // 
+            // lbl_capnhatphong
+            // 
+            this.lbl_capnhatphong.AutoSize = true;
+            this.lbl_capnhatphong.Location = new System.Drawing.Point(527, 334);
+            this.lbl_capnhatphong.Name = "lbl_capnhatphong";
+            this.lbl_capnhatphong.Size = new System.Drawing.Size(38, 13);
+            this.lbl_capnhatphong.TabIndex = 50;
+            this.lbl_capnhatphong.Text = "Phòng";
+            // 
+            // cb_capnhatmadean
+            // 
+            this.cb_capnhatmadean.FormattingEnabled = true;
+            this.cb_capnhatmadean.Location = new System.Drawing.Point(603, 110);
+            this.cb_capnhatmadean.Name = "cb_capnhatmadean";
+            this.cb_capnhatmadean.Size = new System.Drawing.Size(166, 21);
+            this.cb_capnhatmadean.TabIndex = 49;
+            // 
+            // lbl_capnhatmadean
+            // 
+            this.lbl_capnhatmadean.AutoSize = true;
+            this.lbl_capnhatmadean.Location = new System.Drawing.Point(527, 114);
+            this.lbl_capnhatmadean.Name = "lbl_capnhatmadean";
+            this.lbl_capnhatmadean.Size = new System.Drawing.Size(53, 13);
+            this.lbl_capnhatmadean.TabIndex = 48;
+            this.lbl_capnhatmadean.Text = "Mã đề án";
+            // 
+            // lbl_themtendean
+            // 
+            this.lbl_themtendean.AutoSize = true;
+            this.lbl_themtendean.Location = new System.Drawing.Point(-1, 181);
+            this.lbl_themtendean.Name = "lbl_themtendean";
+            this.lbl_themtendean.Size = new System.Drawing.Size(57, 13);
+            this.lbl_themtendean.TabIndex = 47;
+            this.lbl_themtendean.Text = "Tên đề án";
+            // 
+            // comboBox10
+            // 
+            this.comboBox10.FormattingEnabled = true;
+            this.comboBox10.Location = new System.Drawing.Point(74, 337);
+            this.comboBox10.Name = "comboBox10";
+            this.comboBox10.Size = new System.Drawing.Size(166, 21);
+            this.comboBox10.TabIndex = 46;
+            // 
+            // lbl_themphong
+            // 
+            this.lbl_themphong.AutoSize = true;
+            this.lbl_themphong.Location = new System.Drawing.Point(-1, 337);
+            this.lbl_themphong.Name = "lbl_themphong";
+            this.lbl_themphong.Size = new System.Drawing.Size(38, 13);
+            this.lbl_themphong.TabIndex = 45;
+            this.lbl_themphong.Text = "Phòng";
+            // 
+            // lbl_themmadean
+            // 
+            this.lbl_themmadean.AutoSize = true;
+            this.lbl_themmadean.Location = new System.Drawing.Point(6, 112);
+            this.lbl_themmadean.Name = "lbl_themmadean";
+            this.lbl_themmadean.Size = new System.Drawing.Size(53, 13);
+            this.lbl_themmadean.TabIndex = 44;
+            this.lbl_themmadean.Text = "Mã đề án";
+            // 
+            // txt_xoamadean
+            // 
+            this.txt_xoamadean.Location = new System.Drawing.Point(332, 109);
+            this.txt_xoamadean.Name = "txt_xoamadean";
+            this.txt_xoamadean.Size = new System.Drawing.Size(165, 20);
+            this.txt_xoamadean.TabIndex = 62;
+            // 
+            // lbl_xoa
+            // 
+            this.lbl_xoa.AutoSize = true;
+            this.lbl_xoa.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_xoa.Location = new System.Drawing.Point(380, 52);
+            this.lbl_xoa.Name = "lbl_xoa";
+            this.lbl_xoa.Size = new System.Drawing.Size(46, 25);
+            this.lbl_xoa.TabIndex = 61;
+            this.lbl_xoa.Text = "Xóa";
+            // 
+            // lbl_xoamadean
+            // 
+            this.lbl_xoamadean.AutoSize = true;
+            this.lbl_xoamadean.Location = new System.Drawing.Point(263, 114);
+            this.lbl_xoamadean.Name = "lbl_xoamadean";
+            this.lbl_xoamadean.Size = new System.Drawing.Size(53, 13);
+            this.lbl_xoamadean.TabIndex = 60;
+            this.lbl_xoamadean.Text = "Mã đề án";
+            // 
+            // lbl_capnhatngaybd
+            // 
+            this.lbl_capnhatngaybd.AutoSize = true;
+            this.lbl_capnhatngaybd.Location = new System.Drawing.Point(527, 270);
+            this.lbl_capnhatngaybd.Name = "lbl_capnhatngaybd";
+            this.lbl_capnhatngaybd.Size = new System.Drawing.Size(72, 13);
+            this.lbl_capnhatngaybd.TabIndex = 65;
+            this.lbl_capnhatngaybd.Text = "Ngày bắt đầu";
+            // 
+            // lbl_themngaybd
+            // 
+            this.lbl_themngaybd.AutoSize = true;
+            this.lbl_themngaybd.Location = new System.Drawing.Point(-1, 280);
+            this.lbl_themngaybd.Name = "lbl_themngaybd";
+            this.lbl_themngaybd.Size = new System.Drawing.Size(72, 13);
+            this.lbl_themngaybd.TabIndex = 63;
+            this.lbl_themngaybd.Text = "Ngày bắt đầu";
+            // 
+            // dtP_themngaybd
+            // 
+            this.dtP_themngaybd.Location = new System.Drawing.Point(74, 276);
+            this.dtP_themngaybd.Name = "dtP_themngaybd";
+            this.dtP_themngaybd.Size = new System.Drawing.Size(166, 20);
+            this.dtP_themngaybd.TabIndex = 66;
+            // 
+            // dtP_capnhatngaybd
+            // 
+            this.dtP_capnhatngaybd.Location = new System.Drawing.Point(605, 270);
+            this.dtP_capnhatngaybd.Name = "dtP_capnhatngaybd";
+            this.dtP_capnhatngaybd.Size = new System.Drawing.Size(164, 20);
+            this.dtP_capnhatngaybd.TabIndex = 67;
+            // 
+            // btn_xoa
+            // 
+            this.btn_xoa.BackgroundImage = global::Quan_ly_NV.Properties.Resources.backgroud;
+            this.btn_xoa.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_xoa.FlatAppearance.BorderSize = 0;
+            this.btn_xoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_xoa.Location = new System.Drawing.Point(345, 402);
+            this.btn_xoa.Name = "btn_xoa";
+            this.btn_xoa.Size = new System.Drawing.Size(116, 38);
+            this.btn_xoa.TabIndex = 68;
+            this.btn_xoa.Text = "Xóa";
+            this.btn_xoa.UseVisualStyleBackColor = true;
+            // 
+            // frm_crud_dean
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(844, 499);
+            this.Controls.Add(this.btn_xoa);
+            this.Controls.Add(this.dtP_capnhatngaybd);
+            this.Controls.Add(this.dtP_themngaybd);
+            this.Controls.Add(this.lbl_capnhatngaybd);
+            this.Controls.Add(this.lbl_themngaybd);
+            this.Controls.Add(this.txt_xoamadean);
+            this.Controls.Add(this.lbl_xoa);
+            this.Controls.Add(this.lbl_xoamadean);
+            this.Controls.Add(this.btn_capnhat);
+            this.Controls.Add(this.btn_them);
+            this.Controls.Add(this.txt_themmadean);
+            this.Controls.Add(this.lbl_capnhat);
+            this.Controls.Add(this.lbl_them);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lbl_capnhattendean);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.lbl_capnhatphong);
+            this.Controls.Add(this.cb_capnhatmadean);
+            this.Controls.Add(this.lbl_capnhatmadean);
+            this.Controls.Add(this.lbl_themtendean);
+            this.Controls.Add(this.comboBox10);
+            this.Controls.Add(this.lbl_themphong);
+            this.Controls.Add(this.lbl_themmadean);
+            this.Name = "frm_crud_dean";
+            this.Text = "frm_crud_dean";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_capnhat;
+        private System.Windows.Forms.Button btn_them;
+        private System.Windows.Forms.TextBox txt_themmadean;
+        private System.Windows.Forms.Label lbl_capnhat;
+        private System.Windows.Forms.Label lbl_them;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbl_capnhattendean;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label lbl_capnhatphong;
+        private System.Windows.Forms.ComboBox cb_capnhatmadean;
+        private System.Windows.Forms.Label lbl_capnhatmadean;
+        private System.Windows.Forms.Label lbl_themtendean;
+        private System.Windows.Forms.ComboBox comboBox10;
+        private System.Windows.Forms.Label lbl_themphong;
+        private System.Windows.Forms.Label lbl_themmadean;
+        private System.Windows.Forms.TextBox txt_xoamadean;
+        private System.Windows.Forms.Label lbl_xoa;
+        private System.Windows.Forms.Label lbl_xoamadean;
+        private System.Windows.Forms.Label lbl_capnhatngaybd;
+        private System.Windows.Forms.Label lbl_themngaybd;
+        private System.Windows.Forms.DateTimePicker dtP_themngaybd;
+        private System.Windows.Forms.DateTimePicker dtP_capnhatngaybd;
+        private System.Windows.Forms.Button btn_xoa;
+    }
+}
---
+++ Quan_ly_NV/frm_truongdean.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quan_ly_NV
+{
+    public partial class frm_truongdean : Form
+    {
+        private Form activeForm;
+        public frm_truongdean()
+        {
+            InitializeComponent();
+        }
+        private void ActivateButton(object btnSender)
+        {
+
+        }
+        private void OpenChildForm(Form childForm, object btnSender)
+        {
+            if (activeForm != null)
+            {
+                ActiveForm.Close();
+            }
+            ActivateButton(btnSender);
+            activeForm = childForm;
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            this.panel_show.Controls.Add(childForm);
+            this.panel_show.Tag = childForm;
+            childForm.Show();
+
+        }
+
+        private void btn_thongtinnhanvien_Click(object sender, EventArgs e)
+        {
+            lbl_tiltle.Text = btn_thongtinnhanvien.Text;
+            OpenChildForm(new frm_thongtinnhanvien(), sender);
+
+        }
+
+        private void btn_thongtinphongban_Click(object sender, EventArgs e)
+        {
+            lbl_tiltle.Text = btn_thongtinphongban.Text;
+            OpenChildForm(new frm_thongtinphongban(), sender);
+        }
+
+        private void btn_thongtindean_Click(object sender, EventArgs e)
+        {
+            lbl_tiltle.Text = btn_thongtindean.Text;
+            OpenChildForm(new frm_thongtindean(), sender);
+        }
+
+        private void btn_thongtinphancong_Click(object sender, EventArgs e)
+        {
+            lbl_tiltle.Text = btn_thongtinphancong.Text;
+            OpenChildForm(new frm_thongtinphancong(), sender);
+        }
+
+        private void btn_crud_dean_Click(object sender, EventArgs e)
+        {
+            lbl_tiltle.Text = btn_crud_dean.Text;
+            OpenChildForm(new frm_crud_dean(), sender);
+        }
+    }
+}
