7f07415317d7221a454f1302a87162d20b1482b5
Nhật Anh <nhatanhdt463@gmail.com>
2024-05-06 10:36:47 +0700

    Update Login Action

--- Quan_ly_NV/Form1.Designer.cs
+++ Quan_ly_NV/Form1.Designer.cs
@@ -35,14 +35,14 @@
             this.imageList2 = new System.Windows.Forms.ImageList(this.components);
             this.ptb = new System.Windows.Forms.PictureBox();
             this.pnl_login = new System.Windows.Forms.Panel();
+            this.cb_role = new System.Windows.Forms.ComboBox();
+            this.lbl_role = new System.Windows.Forms.Label();
             this.btn_Enter = new System.Windows.Forms.Button();
             this.txt_password = new System.Windows.Forms.TextBox();
             this.lbl_password = new System.Windows.Forms.Label();
             this.txt_username = new System.Windows.Forms.TextBox();
             this.lbl_username = new System.Windows.Forms.Label();
             this.lbl_welcome = new System.Windows.Forms.Label();
-            this.lbl_role = new System.Windows.Forms.Label();
-            this.cb_role = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.ptb)).BeginInit();
             this.pnl_login.SuspendLayout();
             this.SuspendLayout();
@@ -64,9 +64,10 @@
             this.ptb.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(194)))));
             this.ptb.Dock = System.Windows.Forms.DockStyle.Right;
             this.ptb.Image = global::Quan_ly_NV.Properties.Resources.teamwork;
-            this.ptb.Location = new System.Drawing.Point(603, 0);
+            this.ptb.Location = new System.Drawing.Point(804, 0);
+            this.ptb.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ptb.Name = "ptb";
-            this.ptb.Size = new System.Drawing.Size(461, 561);
+            this.ptb.Size = new System.Drawing.Size(615, 690);
             this.ptb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.ptb.TabIndex = 0;
             this.ptb.TabStop = false;
@@ -84,85 +85,10 @@
             this.pnl_login.Controls.Add(this.lbl_welcome);
             this.pnl_login.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnl_login.Location = new System.Drawing.Point(0, 0);
+            this.pnl_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnl_login.Name = "pnl_login";
-            this.pnl_login.Size = new System.Drawing.Size(399, 561);
+            this.pnl_login.Size = new System.Drawing.Size(532, 690);
             this.pnl_login.TabIndex = 1;
-            // 
-            // btn_Enter
-            // 
-            this.btn_Enter.BackgroundImage = global::Quan_ly_NV.Properties.Resources.backgroud;
-            this.btn_Enter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btn_Enter.FlatAppearance.BorderSize = 0;
-            this.btn_Enter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.btn_Enter.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_Enter.Location = new System.Drawing.Point(132, 469);
-            this.btn_Enter.Name = "btn_Enter";
-            this.btn_Enter.Size = new System.Drawing.Size(116, 44);
-            this.btn_Enter.TabIndex = 5;
-            this.btn_Enter.Text = "Đăng nhập";
-            this.btn_Enter.UseVisualStyleBackColor = true;
-            // 
-            // txt_password
-            // 
-            this.txt_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(125)))), ((int)(((byte)(129)))));
-            this.txt_password.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_password.Location = new System.Drawing.Point(48, 298);
-            this.txt_password.Multiline = true;
-            this.txt_password.Name = "txt_password";
-            this.txt_password.Size = new System.Drawing.Size(315, 44);
-            this.txt_password.TabIndex = 4;
-            // 
-            // lbl_password
-            // 
-            this.lbl_password.AutoSize = true;
-            this.lbl_password.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_password.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lbl_password.Location = new System.Drawing.Point(48, 251);
-            this.lbl_password.Name = "lbl_password";
-            this.lbl_password.Size = new System.Drawing.Size(91, 25);
-            this.lbl_password.TabIndex = 3;
-            this.lbl_password.Text = "Mật khẩu";
-            // 
-            // txt_username
-            // 
-            this.txt_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(125)))), ((int)(((byte)(129)))));
-            this.txt_username.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txt_username.Location = new System.Drawing.Point(48, 192);
-            this.txt_username.Multiline = true;
-            this.txt_username.Name = "txt_username";
-            this.txt_username.Size = new System.Drawing.Size(315, 44);
-            this.txt_username.TabIndex = 2;
-            // 
-            // lbl_username
-            // 
-            this.lbl_username.AutoSize = true;
-            this.lbl_username.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_username.Location = new System.Drawing.Point(48, 145);
-            this.lbl_username.Name = "lbl_username";
-            this.lbl_username.Size = new System.Drawing.Size(137, 25);
-            this.lbl_username.TabIndex = 1;
-            this.lbl_username.Text = "Tên đăng nhập";
-            // 
-            // lbl_welcome
-            // 
-            this.lbl_welcome.AutoSize = true;
-            this.lbl_welcome.Font = new System.Drawing.Font("Segoe Print", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_welcome.Location = new System.Drawing.Point(46, 42);
-            this.lbl_welcome.Name = "lbl_welcome";
-            this.lbl_welcome.Size = new System.Drawing.Size(328, 94);
-            this.lbl_welcome.TabIndex = 0;
-            this.lbl_welcome.Text = "WELCOME";
-            // 
-            // lbl_role
-            // 
-            this.lbl_role.AutoSize = true;
-            this.lbl_role.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl_role.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.lbl_role.Location = new System.Drawing.Point(48, 369);
-            this.lbl_role.Name = "lbl_role";
-            this.lbl_role.Size = new System.Drawing.Size(67, 25);
-            this.lbl_role.TabIndex = 6;
-            this.lbl_role.Text = "Vai trò";
             // 
             // cb_role
             // 
@@ -171,24 +97,111 @@
             this.cb_role.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cb_role.ForeColor = System.Drawing.SystemColors.InfoText;
             this.cb_role.FormattingEnabled = true;
-            this.cb_role.Location = new System.Drawing.Point(46, 413);
+            this.cb_role.Location = new System.Drawing.Point(61, 508);
+            this.cb_role.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.cb_role.Name = "cb_role";
-            this.cb_role.Size = new System.Drawing.Size(316, 33);
+            this.cb_role.Size = new System.Drawing.Size(420, 40);
             this.cb_role.TabIndex = 7;
+            // 
+            // lbl_role
+            // 
+            this.lbl_role.AutoSize = true;
+            this.lbl_role.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_role.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl_role.Location = new System.Drawing.Point(64, 454);
+            this.lbl_role.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_role.Name = "lbl_role";
+            this.lbl_role.Size = new System.Drawing.Size(82, 32);
+            this.lbl_role.TabIndex = 6;
+            this.lbl_role.Text = "Vai trò";
+            // 
+            // btn_Enter
+            // 
+            this.btn_Enter.BackgroundImage = global::Quan_ly_NV.Properties.Resources.backgroud;
+            this.btn_Enter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btn_Enter.FlatAppearance.BorderSize = 0;
+            this.btn_Enter.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_Enter.Font = new System.Drawing.Font("Segoe UI Semibold", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Enter.Location = new System.Drawing.Point(176, 577);
+            this.btn_Enter.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_Enter.Name = "btn_Enter";
+            this.btn_Enter.Size = new System.Drawing.Size(155, 54);
+            this.btn_Enter.TabIndex = 5;
+            this.btn_Enter.Text = "Đăng nhập";
+            this.btn_Enter.UseVisualStyleBackColor = true;
+            this.btn_Enter.Click += new System.EventHandler(this.btn_Enter_Click);
+            // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(125)))), ((int)(((byte)(129)))));
+            this.txt_password.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(64, 367);
+            this.txt_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_password.Multiline = true;
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(419, 53);
+            this.txt_password.TabIndex = 4;
+            // 
+            // lbl_password
+            // 
+            this.lbl_password.AutoSize = true;
+            this.lbl_password.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_password.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.lbl_password.Location = new System.Drawing.Point(64, 309);
+            this.lbl_password.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_password.Name = "lbl_password";
+            this.lbl_password.Size = new System.Drawing.Size(115, 32);
+            this.lbl_password.TabIndex = 3;
+            this.lbl_password.Text = "Mật khẩu";
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(194)))), ((int)(((byte)(125)))), ((int)(((byte)(129)))));
+            this.txt_username.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(64, 236);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_username.Multiline = true;
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(419, 53);
+            this.txt_username.TabIndex = 2;
+            // 
+            // lbl_username
+            // 
+            this.lbl_username.AutoSize = true;
+            this.lbl_username.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_username.Location = new System.Drawing.Point(64, 178);
+            this.lbl_username.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_username.Name = "lbl_username";
+            this.lbl_username.Size = new System.Drawing.Size(174, 32);
+            this.lbl_username.TabIndex = 1;
+            this.lbl_username.Text = "Tên đăng nhập";
+            // 
+            // lbl_welcome
+            // 
+            this.lbl_welcome.AutoSize = true;
+            this.lbl_welcome.Font = new System.Drawing.Font("Segoe Print", 39.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_welcome.Location = new System.Drawing.Point(61, 52);
+            this.lbl_welcome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lbl_welcome.Name = "lbl_welcome";
+            this.lbl_welcome.Size = new System.Drawing.Size(416, 118);
+            this.lbl_welcome.TabIndex = 0;
+            this.lbl_welcome.Text = "WELCOME";
             // 
             // frm_login
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(191)))), ((int)(((byte)(125)))), ((int)(((byte)(129)))));
             this.BackgroundImage = global::Quan_ly_NV.Properties.Resources.backgroud;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1064, 561);
+            this.ClientSize = new System.Drawing.Size(1419, 690);
             this.Controls.Add(this.pnl_login);
             this.Controls.Add(this.ptb);
             this.DoubleBuffered = true;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frm_login";
             this.Text = "Đăng nhập";
+            this.Load += new System.EventHandler(this.frm_login_Load);
             ((System.ComponentModel.ISupportInitialize)(this.ptb)).EndInit();
             this.pnl_login.ResumeLayout(false);
             this.pnl_login.PerformLayout();
--- Quan_ly_NV/frm_login.cs
+++ Quan_ly_NV/frm_login.cs
@@ -2,21 +2,114 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.StartPanel;
 
 namespace Quan_ly_NV
 {
     public partial class frm_login : Form
     {
+        private string connectionString = "Data Source=.;Initial Catalog=QUANLYNV;Integrated Security=True; MultipleActiveResultSets=True;Trusted_Connection=True;trustServerCertificate=True;";
         public frm_login()
         {
             InitializeComponent();
         }
 
-        
+        private void btn_Enter_Click(object sender, EventArgs e)
+        {
+            string loginStatus = "";
+            string username = txt_username.Text.ToString();
+            string password = txt_password.Text.ToString();
+            byte[] passwordHash = GetMD5Hash(password);
+            string role = cb_role.SelectedValue?.ToString();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    using (SqlCommand cmd = new SqlCommand("SP_LOGIN_NHANVIEN", connection))
+                    {
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                        cmd.Parameters.Add("@USERNAME", System.Data.SqlDbType.VarChar, 100).Value = username;
+                        cmd.Parameters.Add("@PASSWORD", System.Data.SqlDbType.VarBinary, 8000).Value = passwordHash;
+                        cmd.Parameters.Add("@ROLE", System.Data.SqlDbType.NVarChar, 100).Value = role;
+
+                        SqlParameter output = new SqlParameter("@LOGINSTATUS", System.Data.SqlDbType.NVarChar, 20);
+                        output.Direction = System.Data.ParameterDirection.Output; // Đặt hướng là OUTPUT
+                        cmd.Parameters.Add(output);
+                        connection.Open();
+                        cmd.ExecuteNonQuery();
+
+                        // Lấy giá trị của tham số OUTPUT
+                        loginStatus = output.Value.ToString();
+                    }
+                }
+                if (loginStatus == "SUCCESS")
+                {
+                    MessageBox.Show("Đăng nhập thành công");
+                    this.Hide();
+                    switch(role)
+                    {
+                        case "DBA":
+                            frm_admin admin = new frm_admin();
+                            admin.Show();
+                            break;
+                        default:
+                            this.Show();
+                            break;
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Thất bại");
+                }
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi đăng nhập: " + ex.Message);
+            }
+        }
+
+        private void frm_login_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM VAITRO";
+                    using (SqlDataAdapter dataAdapter = new SqlDataAdapter(query, connection))
+                    {
+                        DataSet dataSet = new DataSet();
+                        dataAdapter.Fill(dataSet, "VAITRO");
+                        cb_role.DataSource = dataSet.Tables["VAITRO"];
+                        cb_role.DisplayMember = "TENVT"; // Thiết lập thành viên hiển thị
+                        cb_role.ValueMember = "TENVT";     // Thiết lập thành viên giá trị
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Lỗi khi tải vai trò: " + ex.Message);
+            }
+        }
+
+        private byte[] GetMD5Hash(string password)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                return hashBytes;
+            }
+        }
+
     }
 }
